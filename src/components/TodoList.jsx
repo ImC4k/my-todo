da9ce122140526c32eb27770b3de3ecbada2bf33
Calvin Chu <calvinchu26@gmail.com>
2020-12-08 16:23:02 +0800

    [todo][Calvin] todo list content

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import TodoGroup from './TodoGroup';
+import TodoGenerator from './TodoGenerator';
 
 class TodoList extends Component {
     render() {
         return (
             <div>
-                
+                <h1>TodoList</h1>
+                <TodoGroup />
+                <TodoGenerator />
             </div>
         );
     }
