5b45f7f771f537fa4e5995fbb8b554df4cd0a604
Calvin Chu <calvinchu26@gmail.com>
2020-12-08 16:27:44 +0800

    [todo][Calvin] feat: implemented todoGroup

--- src/components/TodoGroup.jsx
+++ src/components/TodoGroup.jsx
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
+import TodoItem from './TodoItem';
 
 export default class TodoGroup extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todoList: [
+                {
+                    "id": "c5eb2e60-a718-409c-a583-9ec78d5123c7",
+                    "text": "hi hi",
+                    "done": false
+                },
+            ]
+        }
+    }
+
     render() {
+        const { todoList } = this.state;
         return (
             <div>
-                
+                {
+                    todoList.map(todoItem => 
+                        <TodoItem key={todoItem.id} data={todoItem} />
+                    )
+                }
             </div>
         )
     }
