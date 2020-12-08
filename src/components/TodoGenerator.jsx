ac769a3bfd5ac12c85f33d168f2eb35d0e9b4fff
Calvin Chu <calvinchu26@gmail.com>
2020-12-08 16:23:36 +0800

    [todo][Calvin] implemented generator without redux

--- src/components/TodoGenerator.jsx
+++ src/components/TodoGenerator.jsx
@@ -1,10 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 export default class TodoGenerator extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todo: '',
+        }
+    }
+    handleTodoChange = (event) => {
+        this.setState({
+            todo: event.target.value,
+        });
+    }
+
+    handleAddNewTodo = () => {
+        // create object: {id, text, done: bool}
+        const newTodoItem = {
+            id: uuidv4(),
+            text: this.state.todo,
+            done: false,
+        };
+        //todo use redux
+        console.log(`${JSON.stringify(newTodoItem, null, 4)}`);
+    }
     render() {
         return (
             <div>
-                
+                <input type="text" name="todo" id="new-todo" className="todo-generator-input" value={this.state.todo} placeholder='input a new todo here' onChange={this.handleTodoChange} />
+                <input type="button" value="add" onClick={this.handleAddNewTodo} />
             </div>
         )
     }
