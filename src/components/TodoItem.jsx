4daa04f2378f9eb63e9fa932bce1d7d9a7741b91
Calvin Chu <calvinchu26@gmail.com>
2020-12-08 16:35:12 +0800

    [todo][Calvin] implemented todoItem without redux

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
 
 export default class TodoItem extends Component {
+    deleteItem = () => {
+        // todo redux delete item with uuid
+        console.log(`going to delete ${this.props.data.id}`);
+    }
+
+    toggleDone = () => {
+        if (this.props.data.done) {
+            console.log("now undone");
+        }
+        console.log("now done");
+    }
+
     render() {
+        const {text, done} = this.props.data;
         return (
             <div>
-                
+                <span>
+                    <p>
+                        {done? 'should be crossed' : 'no cross'}
+                    </p>
+                    <p onClick={this.toggleDone}>
+                        {text}
+                    </p>
+                    <button onClick={this.deleteItem}>x</button>
+                </span>
             </div>
         )
     }
