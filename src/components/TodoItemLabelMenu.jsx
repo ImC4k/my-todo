cbdeb4cc9ef82125ac58bff03bab0ac43d377584
Calvin Chu <calvinchu26@gmail.com>
2020-12-09 20:27:27 +0800

    [label][Calvin] update api todos on new label assignment

--- src/components/TodoItemLabelMenu.jsx
+++ src/components/TodoItemLabelMenu.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Menu } from 'antd';
+import { updateTodo } from '../api/todoList.service';
 
-import {PlusCircleOutlined} from '@ant-design/icons'
+import { PlusCircleOutlined } from '@ant-design/icons'
 
 export default class TodoItemLabelMenu extends Component {
     handleAddNewLabel = () => {
@@ -9,28 +10,32 @@ export default class TodoItemLabelMenu extends Component {
     }
 
     getTodoItemInRedux = () => {
-        const {targetTodoItemId: targetId, todoList} = this.props;
+        const { targetTodoItemId: targetId, todoList } = this.props;
         return todoList.filter(todoItem => todoItem.id === targetId)[0];
     }
 
     handleLabelClick = (selectedLabel) => {
         const targetTodoItem = this.getTodoItemInRedux();
-        if (targetTodoItem.labels.map(labelInTodoItem=>labelInTodoItem.id).includes(selectedLabel.id)) {
+        if (targetTodoItem.labels.map(labelInTodoItem => labelInTodoItem.id).includes(selectedLabel.id)) {
             this.props.removeLabel(targetTodoItem.id, selectedLabel);
+            const updatedLabelList = targetTodoItem.labels.filter(label => label.id !== selectedLabel.id);
+            updateTodo({ ...targetTodoItem, labels: updatedLabelList });
         }
         else {
             this.props.addLabel(targetTodoItem.id, selectedLabel);
+            const updatedLabelList = [...targetTodoItem.labels, selectedLabel];
+            updateTodo({ ...targetTodoItem, labels: updatedLabelList });
         }
     }
 
     render() {
-        const {labels} = this.props;
+        const { labels } = this.props;
         return (
             <Menu>
                 <Menu.Item key="add" onClick={this.handleAddNewLabel}>new label <PlusCircleOutlined /></Menu.Item>
                 {
                     labels.map(label =>
-                        <Menu.Item key={label.id} onClick={()=>this.handleLabelClick(label)}>
+                        <Menu.Item key={label.id} onClick={() => this.handleLabelClick(label)}>
                             {label.text}
                         </Menu.Item>
                     )
