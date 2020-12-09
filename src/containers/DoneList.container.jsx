ba1c1b15670b902c05b209db30c90b44053b9601
Calvin Chu <calvinchu26@gmail.com>
2020-12-09 11:27:18 +0800

    [router][Calvin] feat: stub container

---
+++ src/containers/DoneList.container.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class DoneListContainer extends Component {
+    render() {
+        return (
+            <div>
+                hello
+            </div>
+        )
+    }
+}
