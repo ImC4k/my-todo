bbcf0906b7a79d91aa91200ef08c205cb5cc1c93
Calvin Chu <calvinchu26@gmail.com>
2020-12-09 21:30:17 +0800

    [label][Calvin] item show label with color

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -36,6 +36,20 @@ export default class TodoItem extends Component {
         this.props.setTargetTodoItemId(this.props.todo.id);
     }
 
+    reverseContrast = (colorInHex) => {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(colorInHex);
+        const colorInRgb = {
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16)
+        };
+        const brightness = Math.round(((parseInt(colorInRgb.r) * 299) +
+                      (parseInt(colorInRgb.g) * 587) +
+                      (parseInt(colorInRgb.b) * 114)) / 1000);
+        console.log(`brightness is ${brightness}`);
+        return (brightness > 126) ? 'black' : 'white';
+    }
+
     render() {
         const { text, done, labels } = this.props.todo;
         return (
@@ -48,7 +62,7 @@ export default class TodoItem extends Component {
                         <div class='labels'>
                             {
                                 labels.map(label => 
-                                    <p className='label' key={label.id} style={{backgroundColor: label.color, color: 'white'}}>
+                                    <p className='label' key={label.id} style={{backgroundColor: label.color, color: this.reverseContrast(label.color)}}>
                                         {label.text}
                                     </p>
                                 )
