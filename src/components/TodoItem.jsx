56eddbccb13bc1cac15c862aacaac294f49bcffc
Calvin Chu <calvinchu26@gmail.com>
2020-12-09 21:54:48 +0800

    [label][Calvin] fix styling

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,5 +1,5 @@
 import { DeleteOutlined } from '@ant-design/icons';
-import { Dropdown } from 'antd';
+import { Dropdown, notification } from 'antd';
 import classNames from 'classnames';
 import React, { Component } from 'react';
 import { deleteTodo, updateTodo } from '../api/todoList.service';
@@ -8,11 +8,21 @@ import TodoItemLabelMenuContainer from '../containers/TodoItemLabelMenu.containe
 
 
 export default class TodoItem extends Component {
-    deleteItem = () => {
+    deleteItem = (event) => {
         deleteTodo(this.props.todo.id)
             .then(_ => {
                 this.props.deleteTodo(this.props.todo.id);
             });
+        event.stopPropagation();
+    }
+
+    showNotification = () => {
+        const {text, done} = this.props.todo;
+        notification.open({
+            message: 'Todo item status updated',
+            description: done? `${text} is marked as not done` : `${text} is marked as done`,
+            duration: 2
+        });
     }
 
     toggleDone = () => {
@@ -23,6 +33,7 @@ export default class TodoItem extends Component {
         };
 
         this.props.updateTodoItem(updatedTodoItem);
+        this.showNotification();
 
         updateTodo(updatedTodoItem)
             .catch(error => {
@@ -33,11 +44,12 @@ export default class TodoItem extends Component {
     }
 
     setTargetTodoItem = () => {
+        console.log('setting target');
         this.props.setTargetTodoItemId(this.props.todo.id);
     }
 
     reverseContrast = (colorInHex) => {
-        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(colorInHex);
+        const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(colorInHex);
         const colorInRgb = {
             r: parseInt(result[1], 16),
             g: parseInt(result[2], 16),
@@ -46,29 +58,30 @@ export default class TodoItem extends Component {
         const brightness = Math.round(((parseInt(colorInRgb.r) * 299) +
                       (parseInt(colorInRgb.g) * 587) +
                       (parseInt(colorInRgb.b) * 114)) / 1000);
-        console.log(`brightness is ${brightness}`);
         return (brightness > 126) ? 'black' : 'white';
     }
 
     render() {
         const { text, done, labels } = this.props.todo;
         return (
-            <div onContextMenu={this.setTargetTodoItem} onClick={this.setTargetTodoItem}>
-                <Dropdown overlay={<TodoItemLabelMenuContainer />} trigger={['contextMenu']}>
+            <div onContextMenu={this.setTargetTodoItem}>
+                <Dropdown overlay={<TodoItemLabelMenuContainer />} trigger={['contextMenu']} onClick={() => {this.toggleDone(); this.setTargetTodoItem();}}>
                     <div className='todo-item'>
-                        <p onClick={this.toggleDone} className={classNames({ 'done': done }, 'todo-text')}>
+                        <p className={classNames({ 'done': done }, 'todo-text')}>
                             {text}
                         </p>
-                        <div class='labels'>
-                            {
-                                labels.map(label => 
-                                    <p className='label' key={label.id} style={{backgroundColor: label.color, color: this.reverseContrast(label.color)}}>
-                                        {label.text}
-                                    </p>
-                                )
-                            }
+                        <div className="meta-content">
+                            <div className='labels'>
+                                {
+                                    labels.map(label => 
+                                        <p className='label' key={label.id} style={{backgroundColor: label.color, color: this.reverseContrast(label.color)}}>
+                                            {label.text}
+                                        </p>
+                                    )
+                                }
+                            </div>
+                            <DeleteOutlined onClick={this.deleteItem} className='delete-item-btn'>x</DeleteOutlined>
                         </div>
-                        <DeleteOutlined onClick={this.deleteItem} className='delete-item-btn'>x</DeleteOutlined>
                     </div>
                 </Dropdown>
             </div>
