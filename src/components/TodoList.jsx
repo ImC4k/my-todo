1948a88529b548b5fd92436fe60f603fada8820d
Calvin Chu <calvinchu26@gmail.com>
2020-12-09 16:55:47 +0800

    [design][Calvin] update: fix todoList header position

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import TodoGroupContainer from '../containers/TodoGroup.container';
 import TodoGeneratorContainer from '../containers/TodoGenerator.container';
 
+import '../styles/TodoList.style.scss';
 class TodoList extends Component {
     render() {
         return (
             <div>
-                <h1>TodoList</h1>
+                <h1 className='header'>TodoList</h1>
                 <TodoGroupContainer />
                 <TodoGeneratorContainer />
             </div>
