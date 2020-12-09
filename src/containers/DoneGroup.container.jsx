c996daf0f5e855f2ef34ba9191bcc40dcade070e
Calvin Chu <calvinchu26@gmail.com>
2020-12-09 11:54:00 +0800

    [router][Calvin] refactor: removed duplicated component

--- src/containers/DoneGroup.container.jsx
+++ src/containers/DoneGroup.container.jsx
@@ -1,9 +1,9 @@
 import {connect} from 'react-redux';
-import DoneGroup from '../components/DoneGroup';
+import TodoGroup from '../components/TodoGroup';
 
 const mapStateToProps = state => ({
     todoList: state.todoList.filter(todoItem => todoItem.done)
 });
 
-const DoneGroupContainer = connect(mapStateToProps)(DoneGroup);
+const DoneGroupContainer = connect(mapStateToProps)(TodoGroup);
 export default DoneGroupContainer;
